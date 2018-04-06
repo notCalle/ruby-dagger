9850e83469f37e56014e0df8e7fb70b976bf5edb
Calle Englund <calle@discord.bofh.se>
2018-04-06 20:27:10 +0200

    Add mixin for named edges
    `Tangle` currently does not have named edges so we need to add that to our mixin.

--- lib/dagger/tangle_mixin.rb
+++ lib/dagger/tangle_mixin.rb
@@ -19,5 +19,17 @@ module Dagger
         @keys = KeyTree.open_all(dir)
       end
     end
+
+    # Tangle::Vertex mixin for initializing a KeyTree::Forest from the contents
+    # of a directory
+    module Edge
+      attr_reader :name
+
+      private
+
+      def initialize_kwarg_name(name)
+        @name = name
+      end
+    end
   end
 end
