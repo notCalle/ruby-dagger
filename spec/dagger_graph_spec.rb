edc3a7cfe921757a2a56ec703227174b6c401235
Calle Englund <calle@discord.bofh.se>
2018-04-06 20:33:43 +0200

    Update graph specs and fixtures

--- spec/dagger_graph_spec.rb
+++ spec/dagger_graph_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Dagger::Graph do
       @graph = Dagger::Graph.new
       @vertex = @graph.add_vertex(name: 'vertex1',
                                   directory: fixture('graph/vertex1'))
-      @expected_values = { 'a.b' => 1, 'c' => 2 }
+      @expected_values = { 'a.b' => 2, 'c' => 3 }
     end
 
     it 'can have keys' do
@@ -28,6 +28,37 @@ RSpec.describe Dagger::Graph do
     it 'can fetch values for keys' do
       @expected_values.each do |key, value|
         expect(@vertex.keys[key]).to eq value
+      end
+    end
+  end
+
+  it 'can load a graph from a directory structure' do
+    expect {
+      Dagger::Graph.new(directory: fixture('graph'))
+    }.not_to raise_error
+  end
+
+  context 'when loading a graph' do
+    before :context do
+      @graph = Dagger::Graph.new(directory: fixture('graph'))
+      @expected = {
+        '/' => {},
+        '/vertex1' => { 'a.b' => 2, 'c' => 3 },
+        '/vertex2' => { 'b' => 2 }
+      }
+    end
+
+    it 'has the expected vertices' do
+      @expected.each_key do |name|
+        expect { @graph.get_vertex(name) }.not_to raise_error
+      end
+    end
+
+    it 'has the expected vertex key/values' do
+      @expected.each do |name, keys|
+        keys.each do |key, value|
+          expect(@graph.get_vertex(name).keys[key]).to eq value
+        end
       end
     end
   end
