392a572e3c293df8237fb4a4104b82a161693fd8
Calle Englund <calle@discord.bofh.se>
2018-05-25 09:39:20 +0200

    Add require_name value generator
    Shortcut for regexp matching requirement on `_meta.name` for further processing of a rule chain.

---
+++ lib/dagger/generator/require_name.rb
@@ -0,0 +1,19 @@
+require_relative '../generator'
+
+module Dagger
+  module Generate
+    # Set requirement for further processing.
+    #
+    # _default.key:
+    #   - require_name: regexp
+    #   - ...
+    class RequireName < Dagger::Generator
+      def yield(regexps)
+        string = dictionary['_meta.name']
+        enumerable(regexps).any? do |regexp|
+          ::Regexp.new(regexp).match?(string)
+        end
+      end
+    end
+  end
+end
