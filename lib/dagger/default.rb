4b22d95659cdb279c47c70faba45db42eef693cc
Calle Englund <calle@discord.bofh.se>
2018-05-26 16:00:43 +0200

    Refactor default value caching and locking
    Split caching and locking out of `#generate` into their own methods, and add fallback dictionary if no value was generated (for deep caching).

--- lib/dagger/default.rb
+++ lib/dagger/default.rb
@@ -11,9 +11,13 @@ module Dagger
     #   new(dictionary) => Dagger::Default
     #   new(*, cached: false)
     #   new(*, rule_prefix: '_default')
-    def initialize(dictionary, cached: false, rule_prefix: '_default')
+    def initialize(dictionary,
+                   cached: false,
+                   fallback: nil,
+                   rule_prefix: '_default')
       @dictionary = dictionary
       @cached = cached
+      @fallback = fallback
       @rule_prefix = KeyTree::Path[rule_prefix]
       @default_proc = ->(tree, key) { generate(tree, key) }
       @locks = Set[]
@@ -32,21 +36,44 @@ module Dagger
 
     # Generate a default value for a +key+, possibly caching the
     # result in the +tree+.
-    # Raises a +KeyError+ f the default value cannot be generated.
     #
     # :call-seq:
     #   generate(tree, key) => value || KeyError
+    #
+    # Raises a +KeyError+ if the default value cannot be generated.
+    # Raises a +RuntimeError+ in case of a deadlock for +key+.
     def generate(tree, key)
       key = KeyTree::Path[key] unless key.is_a? KeyTree::Path
-      raise %(deadlock detected: "#{key}") unless @locks.add?(key)
 
-      return result = process(key) unless cached?
-      tree[key] = result unless result.nil?
+      with_locked_key(key) { |locked_key| cached_value(tree, locked_key) }
+    end
+
+    private
+
+    # Process a +block+ with mutex for +key+
+    #
+    # :call-seq:
+    #   with_locked_key(key &->(locked_key)) => result
+    def with_locked_key(key)
+      raise %(deadlock detected: "#{key}") unless @locks.add?(key)
+      yield(key)
     ensure
       @locks.delete(key)
     end
 
-    private
+    # Return the default value for +key+, caching it in +tree+ if enabled.
+    #
+    # :call-seq:
+    #   cached_value(tree, key) => value || KeyError
+    def cached_value(tree, key)
+      result = process(key)
+    rescue KeyError
+      raise if @fallback.nil?
+      result = @fallback[key]
+      raise if result.nil?
+    ensure
+      tree[key] = result if cached? && !result.nil?
+    end
 
     # Process value generation rules for +context+, raising a +KeyError+
     # if a value could not be generated. Catches :result thows from rule
