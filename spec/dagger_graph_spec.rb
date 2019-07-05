fd2de565503e8afa366f01e103d671ef8e3226e8
Calle Englund <calle@discord.bofh.se>
2019-07-05 21:26:00 +0200

    Add specs for loading graphs

--- spec/dagger_graph_spec.rb
+++ spec/dagger_graph_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Dagger::Graph do
   it 'is a specialization of Tangle::DAG' do
     expect(Dagger::Graph.new).to be_a Tangle::DAG
   end
+
+  it 'can load a graph from a directory' do
+    expect { @graph = Dagger.load(fixture('graph')) }.not_to raise_error
+    expect(@graph.vertices.count).to eq 3
+  end
+
+  it 'can load a graph from a directory with a config file' do
+    expect { @graph = Dagger.load(fixture('options')) }.not_to raise_error
+    expect(@graph.vertices.count).to eq 1
+  end
+
+  context 'when loading a graph with a config file' do
+    it 'cannot escape the original root directory' do
+      expect {
+        @graph = Dagger.load(fixture('escape'))
+      }.to raise_error RuntimeError, '../graph is outside root'
+      expect(@graph).to be_nil
+    end
+  end
 end
